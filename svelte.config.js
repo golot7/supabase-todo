9edf68f4ce8008cc0c2dac2b85caa8db8a5f2f18
golot7 <72505434+golot7@users.noreply.github.com>
2022-03-20 00:30:23 +0100

    Update svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -3,8 +3,12 @@ import adapter from '@sveltejs/adapter-auto';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter(),
-
+		adapter: adapter(),	
+		vite: {
+      			define: {
+        			'process.env': process.env,
+      			},
+    		},
 		// hydrate the <div id="svelte"> element in src/app.html
 		target: '#svelte'
 	}
